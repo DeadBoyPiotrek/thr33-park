11d4a150664d1e6d747393e80e9f7b2773cae7c9
DeadBoyPiotrek <87661735+DeadBoyPiotrek@users.noreply.github.com>
2022-05-25 21:55:06 +0200

    feat: add navigation

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react';
 import Navigation from '../components/navigation/Navigation';
-// import Scene from '../components/scene/Scene';
+import Scene from '../components/scene/Scene';
 import styles from './index.module.scss';
 export default function Home() {
   return (
     <div className={styles.container}>
       <Navigation />
       <Suspense fallback={null}>
-        <Canvas className={styles.container__canvas}>{/* <Scene /> */}</Canvas>
+        <Canvas className={styles.container__canvas}>
+          <Scene />
+        </Canvas>
       </Suspense>
     </div>
   );
