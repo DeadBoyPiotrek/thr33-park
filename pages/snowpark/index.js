ef38cf4051da191c1e875fb8fc68c92964435ec8
piotrek <pipolito2115@gmail.com>
2022-07-13 02:31:09 +0200

    feat: get data from openweather

--- pages/snowpark/index.js
+++ pages/snowpark/index.js
@@ -4,22 +4,32 @@ import styles from './snowpark.module.scss';
 
 export const getStaticProps = async () => {
   const weather = await getCurrentWeather();
-
+  const props = {
+    time: new Date().toLocaleTimeString(),
+    city: weather.name,
+    temperature: weather.main.temp,
+    weather: weather.weather[0].main,
+    weatherDescription: weather.weather[0].description,
+    weatherIcon: weather.weather[0].icon,
+  };
   return {
     props: {
-      weather,
+      props,
     },
     revalidate: 6,
   };
 };
 
-function Snowpark({ weather }) {
+function Snowpark({ props }) {
   return (
     <>
       <div className={styles.wrapper}>
-        {/* {JSON.stringify(weather)} */}
-        <div>Pogoda na stoku</div>
-        <div></div>
+        <div className={styles.snowpark}>
+          <section className={styles.snowpark__section}>
+            <h2>Pogoda na stoku</h2>
+            <pre>{JSON.stringify(props, undefined, 2)}</pre>
+          </section>
+        </div>
       </div>
     </>
   );
